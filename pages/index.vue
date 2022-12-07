eb5441aca3f91c955710323763658abbeee54f02
limtom <horlartom@live.com>
2022-12-07 21:18:49 +0100

    nuxt-link

--- pages/index.vue
+++ pages/index.vue
@@ -28,45 +28,48 @@
           show-arrows
         >
           <v-slide-item v-for="(product, i) in slideItems" :key="i">
-            <v-card class="ma-4" width="250">
-              <v-img
-                height="150"
-                :src="product.image_url"
-                :alt="product.product_name"
-                contain
-              ></v-img>
+            <nuxt-link :to="`/${product.id}`" class="text-decoration-none">
+              <v-card class="ma-4" width="250">
+                <v-img
+                  height="150"
+                  :src="product.image_url"
+                  :alt="product.product_name"
+                  contain
+                ></v-img>
 
-              <v-card-title class="text-subtitle-2 pa-2 text-truncate">
-                {{ product.product_name }}
-              </v-card-title>
+                <v-card-title class="text-subtitle-2 pa-2 text-truncate">
+                  {{ product.product_name }}
+                </v-card-title>
 
-              <v-card-text class="pa-5">
-                <div
-                  class="price-text text-center blue--text font-weight-light"
-                >
-                  <span class="">&#8358;</span>
-                  {{ product.price
-                  }}<span class="ml-2 text-caption">Per Carton</span>
-                </div>
-              </v-card-text>
-              <v-card-text class="pa-0 pl-16 pr-16"> </v-card-text>
-              <v-card-actions class="pt-5 pb-5 justify-center">
-                <v-btn
-                  color="blue darken-1"
-                  small
-                  outlined
-                  class="text-capitalize snipcart-add-item pl-10 pr-10 block"
-                  @click="addToCart(product)"
-                  :data-item-id="product.id"
-                  :data-item-name="product.product_name"
-                  :data-item-price="product.price"
-                  :data-item-image="product.image_url"
-                  :data-item-description="product.description"
-                >
-                  Add to cart
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+                <v-card-text class="pa-5">
+                  <div
+                    class="price-text text-center blue--text font-weight-light"
+                  >
+                    <span class="">&#8358;</span>
+                    {{ product.price
+                    }}<span class="ml-2 text-caption">Per Carton</span>
+                  </div>
+                </v-card-text>
+                <v-card-text class="pa-0 pl-16 pr-16"> </v-card-text>
+                <v-card-actions class="pt-5 pb-5 justify-center">
+                  <v-btn
+                    color="blue darken-1"
+                    small
+                    outlined
+                    class="text-capitalize snipcart-add-item pl-10 pr-10 block"
+                    @click="addToCart(product)"
+                    :data-item-id="product.id"
+                    :data-item-name="product.product_name"
+                    :data-item-price="product.price"
+                    :data-item-image="product.image_url"
+                    :data-item-description="product.description"
+                    :data-item-url="`/${product.id}`"
+                  >
+                    Add to cart
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </nuxt-link>
           </v-slide-item>
         </v-slide-group>
       </v-sheet></v-col
